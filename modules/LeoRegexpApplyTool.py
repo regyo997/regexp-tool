716b1e4a1cbeecd736081fdf087b602a70e6a72f
LeoLin <loe.lin@varmeego.com>
2021-01-15 16:22:36 +0800

    refine Methods - rename and rewrap

--- modules/LeoRegexpApplyTool.py
+++ modules/LeoRegexpApplyTool.py
@@ -26,13 +26,18 @@ class LeoRegexpApplyTool:
     def _doRegProcess(self, regexp: LeoRegexp, inputText: str):
         outputText = inputText
         for regexpMatch in re.finditer(regexp.match, outputText, re.DOTALL):
-            if(regexpMatch != None):
-                outputText = self._replaceOrNot(
-                    regexp, regexpMatch, outputText)
+            self._geneateOutputText(regexp, regexpMatch, outputText)
         return outputText
 
-    def _replaceOrNot(self, regexp: LeoRegexp, regexpMatch: List[re.Match], inputText: str):
-        outputText=inputText
+    def _geneateOutputText(self, regexp: LeoRegexp, regexpMatch: List[re.Match], inputText: str):
+        outputText = inputText
+        if(regexpMatch != None):
+            outputText = self._replaceTextOrNot(
+                regexp, regexpMatch, outputText)
+        return outputText
+
+    def _replaceTextOrNot(self, regexp: LeoRegexp, regexpMatch: List[re.Match], inputText: str):
+        outputText = inputText
         if(regexp.replace != None):
             outputText = self._generateReplaceText(
                 regexp, regexpMatch, outputText)
